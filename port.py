1d682c4f46a8890141f94340c57d6f272069c565
Vengatesan K <128688827+Vengatesan-K@users.noreply.github.com>
2023-10-14 11:45:43 +0530

    Update port.py

--- port.py
+++ port.py
@@ -184,13 +184,13 @@ with col1:
                 unsafe_allow_html=True)
 
  contact_form = """
-     <form action="https://formsubmit.co/YOUREMAIL@EMAIL.COM" method="POST">
-     <input type="hidden" name="_captcha" value="false">
-     <input type="text" name="name" placeholder="Your name" required>
-     <input type="email" name="email" placeholder="Your email" required>
-     <textarea name="message" placeholder="Your message here"></textarea>
-     <button type="submit">Send</button>
-     </form>
+ <form action="https://formsubmit.co/YOUREMAIL@EMAIL.COM" method="POST">
+ <input type="hidden" name="_captcha" value="false">
+ <input type="text" name="name" placeholder="Your name" required>
+ <input type="email" name="email" placeholder="Your email" required>
+ <textarea name="message" placeholder="Your message here"></textarea>
+ <button type="submit">Send</button>
+ </form>
      """
 
  st.markdown(contact_form, unsafe_allow_html=True)
