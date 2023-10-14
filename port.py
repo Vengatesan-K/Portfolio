c8b1fa76ee76cc39fa351dbe671b42dbd3a6446f
Vengatesan K <128688827+Vengatesan-K@users.noreply.github.com>
2023-10-14 11:15:59 +0530

    Update port.py

--- port.py
+++ port.py
@@ -47,7 +47,7 @@ with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
 
-col1,col2 = st.columns([4,6])
+col1,col2 = st.columns([2,8])
 with col1:
 #####################
 # Header 
