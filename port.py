158a8481a1ea9131b40ddf68d7c38439c54a9c10
Vengatesan K <128688827+Vengatesan-K@users.noreply.github.com>
2023-10-14 11:36:19 +0530

    Update port.py

--- port.py
+++ port.py
@@ -180,7 +180,28 @@ with col1:
  fig6.update_traces(textinfo='none')
  fig6.update_layout(width=300, height=300)
  st.plotly_chart(fig6,use_container_width=True)
- 
+ st.markdown('__<p style="text-align:left; font-size: 20px; color: #F7F5F5">:mailbox: Get In Touch With Me!</P>__',
+                unsafe_allow_html=True)
+
+     contact_form = """
+     <form action="https://formsubmit.co/YOUREMAIL@EMAIL.COM" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
+     </form>
+     """
+
+     st.markdown(contact_form, unsafe_allow_html=True)
+
+# Use Local CSS File
+     def local_css(file_name):
+        with open(file_name) as f:
+          st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+     local_css("style1.css") 
 
 
 
